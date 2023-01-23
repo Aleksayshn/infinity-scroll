9a4e34c97486e69743a41646412e6e13266e7856
Aleksandra <ag7756862@gmail.com>
2023-01-23 19:01:57 +0200

    feat: add IntersectionObserver

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 import { PixabayAPI } from './js/api';
 import { createMarkUpGallery } from './js/markup';
+import { observer } from './js/infinity-scroll';
 
-const lightbox = new SimpleLightbox(".gallery a", {
+export const lightbox = new SimpleLightbox(".gallery a", {
     captionsData: "alt",
     captionDelay: 250,
 });
@@ -16,21 +17,18 @@ const lightbox = new SimpleLightbox(".gallery a", {
 const refs = {
     searchFormEl: document.querySelector('#js-search-form'),
     galleryListEl: document.querySelector('.js-gallery'),
-    loadMoreBtnEl: document.querySelector('.js-load-more'),
     searchBtnEl: document.querySelector('.js-search-btn'),
     endCollectionText: document.querySelector(".end-collection-text"),
+    targetScrollEl: document.querySelector(".target-element")
 }
 
-const { searchFormEl, galleryListEl, loadMoreBtnEl, searchBtnEl, endCollectionText } = refs;
-const pixabayAPI = new PixabayAPI();
+export const { searchFormEl, galleryListEl, searchBtnEl, targetScrollEl, endCollectionText } = refs;
+export const pixabayAPI = new PixabayAPI();
 
 searchFormEl.addEventListener('submit', onSearchFormSubmit);
-loadMoreBtnEl.addEventListener('click', onLoadMoreBtnClick);
 
 async function onSearchFormSubmit(e) {
     e.preventDefault();
-    loadMoreBtnEl.classList.add('is-hidden');
-    endCollectionText.classList.add("is-hidden");
     pixabayAPI.query = e.target.elements.searchQuery.value.trim().toLowerCase();
     pixabayAPI.page = 1;
 
@@ -49,56 +47,23 @@ async function onSearchFormSubmit(e) {
             galleryListEl.innerHTML = '';
             return;
         }
-        console.log(data);
+
         Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
         galleryListEl.innerHTML = createMarkUpGallery(data.hits);
         lightbox.refresh();
+        setTimeout(() => { // помещаем функцию отсеживания в макроочередь, чтобы 
+            // отслеживание вызывалась только после рендера/отрисовывания img
+            observer.observe(targetScrollEl) // Выбор отслеживаемого элемента
+        }, 100); //ставим задержку чтобі успела отрисоваться страница
 
         if (data.totalHits > pixabayAPI.per_page) {
-            loadMoreBtnEl.classList.remove('is-hidden');
             return
         }
         endCollectionText.classList.remove("is-hidden");
     }
-
     catch (error) {
         console.log(error);
     }
-    finally {
-        searchBtnEl.disabled = false;
-    };
+    searchBtnEl.disabled = false;
 }
 
-async function onLoadMoreBtnClick(e) {
-    e.target.disabled = true;
-    pixabayAPI.page += 1;
-
-    try {
-        const { data } = await pixabayAPI.fetchImagePixabay();
-        galleryListEl.insertAdjacentHTML('beforeend', createMarkUpGallery(data.hits));
-        lightbox.refresh();
-
-        const { height: cardHeight } = document
-            .querySelector('.gallery')
-            .firstElementChild.getBoundingClientRect();
-
-        window.scrollBy({
-            top: cardHeight * 2,
-            behavior: 'smooth',
-        });
-
-        if (data.totalHits < pixabayAPI.per_page * pixabayAPI.page) {
-            loadMoreBtnEl.classList.add('is-hidden');
-            endCollectionText.classList.remove("is-hidden");
-        }
-    }
-
-    catch (error) {
-        console.log(error);
-    }
-
-    finally {
-        e.target.disabled = false;
-    }
-
-}
--- src/js/infinity-scroll.js
+++ src/js/infinity-scroll.js
@@ -1,33 +1,43 @@
-export function onLoadMoreBtnClick() {
+'use strict'
 
-    if (this.scrollTop + this.clientHeight >= this.scrollHeight) {
-        pixabayAPI.page += 1;
-        console.log(pixabayAPI.page);
+import { lightbox } from "../index";
+import { galleryListEl } from "../index";
+import { pixabayAPI } from "../index";
+import { createMarkUpGallery, targetScrollEl, endCollectionText } from './markup';
 
-
-        pixabayAPI.fetchImagePixabay()
-            .then(data => {
-                galleryListEl.insertAdjacentHTML('beforeend', createMarkUpGallery(data.hits));
-                lightbox.refresh();
-                const { height: cardHeight } = document
-                    .querySelector('.gallery')
-                    .firstElementChild.getBoundingClientRect();
-
-                window.scrollBy({
-                    top: cardHeight * 2,
-                    behavior: 'smooth',
-                });
-
-                if (data.totalHits < pixabayAPI.per_page * pixabayAPI.page) {
-                    Notiflix.Notify.info(
-                        "We're sorry, but you've reached the end of search results."
-                    );
-                }
-            })
-            .catch(
-                err => {
-                    console.log(err);
-                }
-            )
+export const observer = new IntersectionObserver((entries) => {
+    if (!entries[0].isIntersecting) {
+        return;
     }
-};
+    onLoadMoreBtnClick()
+}, {
+    root: null,
+    rootMargin: '0px 0px 250px 0px',
+    threshold: 1.0,
+});
+
+async function onLoadMoreBtnClick() {
+    pixabayAPI.page += 1;
+    try {
+        const { data } = await pixabayAPI.fetchImagePixabay();
+        galleryListEl.insertAdjacentHTML('beforeend', createMarkUpGallery(data.hits));
+        lightbox.refresh();
+
+        const { height: cardHeight } = document
+            .querySelector('.gallery')
+            .firstElementChild.getBoundingClientRect();
+
+        window.scrollBy({
+            top: cardHeight * 2,
+            behavior: 'smooth',
+        });
+
+        if (data.totalHits < pixabayAPI.per_page * pixabayAPI.page) {
+            observer.unobserve(targetScrollEl); //убираем элемент из слежения
+            endCollectionText.classList.remove("is-hidden");
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
